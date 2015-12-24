49cf399f439b0cb4b17e334130ab6d8856fdcb37
yangfeng <yangfeng@qq.com>
2015-12-24 23:37:41 +0800

    添加网络工具单例，设置缓存，以及知识器

---
+++ 新闻/新闻/Class/Tools/NetworkTools.h
@@ -0,0 +1,16 @@
+//
+//  NetworkTools.h
+//  新闻
+//
+//  Created by yangfeng on 15/12/24.
+//  Copyright © 2015年 yangfeng. All rights reserved.
+//
+
+#import "AFHTTPSessionManager.h"
+
+@interface NetworkTools : AFHTTPSessionManager
+
+
++ (instancetype)shareNetWorkTools;
+
+@end
---
+++ 新闻/新闻/Class/Tools/NetworkTools.m
@@ -0,0 +1,34 @@
+//
+//  NetworkTools.m
+//  新闻
+//
+//  Created by yangfeng on 15/12/24.
+//  Copyright © 2015年 yangfeng. All rights reserved.
+//
+
+#import "NetworkTools.h"
+
+@implementation NetworkTools
+
++ (instancetype)shareNetWorkTools{
+    
+    
+    static NetworkTools *instace;
+    
+    static dispatch_once_t onceToken;
+    
+    dispatch_once(&onceToken, ^{
+        
+        NSURL *url = [NSURL URLWithString:@"http://c.m.163.com/nc/article/headline/"];
+        
+        instace = [[self alloc] initWithBaseURL:url];
+        
+    });
+    
+    
+    return instace;
+    
+}
+
+
+@end
