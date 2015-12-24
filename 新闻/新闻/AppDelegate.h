23745a892bedf62f413d6f2b62edc373625c3789
yangfeng <yangfeng@qq.com>
2015-12-24 23:02:29 +0800

    初始化项目

---
+++ 新闻/新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  新闻
+//
+//  Created by yangfeng on 15/12/24.
+//  Copyright © 2015年 yangfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 新闻/新闻/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  新闻
+//
+//  Created by yangfeng on 15/12/24.
+//  Copyright © 2015年 yangfeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
